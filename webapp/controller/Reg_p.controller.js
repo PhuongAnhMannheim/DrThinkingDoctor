2ad159ad3844f01ea36c7e6e52dff000b46268c2
D073477 <qian.xia@sap.com>
2018-10-13 13:29:31 +0000

    reg

---
+++ webapp/controller/Reg_p.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define(['sap/m/MessageToast','sap/ui/core/mvc/Controller'],
+	function(MessageToast, Controller) {
+	"use strict";
+
+	var ControllerController = Controller.extend("test1.controller.Reg_p", {
+		
+	});
+
+
+
+});
