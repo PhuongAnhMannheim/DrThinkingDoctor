e49116ffb1e6dd8ff3aac8fddd37474e4517e1ee
D064966 <phuong.le@sap.com>
2018-10-14 16:28:28 +0000

    chatroom

---
+++ webapp/controller/Chatroom.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/m/MessageToast",
+	"sap/ui/core/mvc/Controller"
+], function(MessageToast, Controller) {
+	"use strict";
+
+	return Controller.extend("DrThinkingDoctor.controller.Chatroom", {
+		
+	});
+});
