ceca0bc2848faa0ed1ac13a88667a4ac404a1fd5
D064966 <phuong.le@sap.com>
2018-10-14 13:14:40 +0000

    add register navigation

--- webapp/controller/Welcome.controller.js
+++ webapp/controller/Welcome.controller.js
@@ -1,9 +1,16 @@
 sap.ui.define([
+	"sap/m/MessageToast",
 	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+], function(MessageToast, Controller) {
 	"use strict";
 
 	return Controller.extend("DrThinkingDoctor.controller.Welcome", {
-
+		onPressRegister: function(oEvent){
+			/*var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("register");*/
+			this.getOwnerComponent().getRouter().navTo("register");
+			/*var msg = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy\r\n eirmod.';
+			MessageToast.show(msg);*/
+		}
 	});
 });
