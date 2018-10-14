5ab400212c3a9c5f380fb17685fde1d77176e9b6
D073477 <qian.xia@sap.com>
2018-10-14 19:57:42 +0000

    profile

--- webapp/controller/Profile.controller.js
+++ webapp/controller/Profile.controller.js
@@ -6,6 +6,15 @@ sap.ui.define([
 	return Controller.extend("DrThinkingDoctor.controller.Profile", {
 		onPressHome: function(oEvent){
 			this.getOwnerComponent().getRouter().navTo("home");
+		},
+		onInit: function () {
+ 
+    
+			var oModel = new sap.ui.model.json.JSONModel("profile.json");
+			
+
+			this.getView().setModel(oModel);
+
 		}
 	});
 });
