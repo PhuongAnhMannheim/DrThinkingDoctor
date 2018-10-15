d5ad036a10f4069c3ed112d3ad7d53ec92be4232
D073477 <qian.xia@sap.com>
2018-10-15 08:47:31 +0000

    profile

--- webapp/controller/Profile.controller.js
+++ webapp/controller/Profile.controller.js
@@ -14,6 +14,24 @@ sap.ui.define([
 
 			this.getView().setModel(oModel);
 
+		},
+		onExit : function () {
+			if (this._oPopover) {
+				this._oPopover.destroy();
+			}
+		},
+
+		_getPopover : function () {
+			if (!this._oPopover) {
+				this._oPopover = sap.ui.xmlfragment("sap.m.sample.ObjectHeaderResponsiveIII.Popover", this);
+				this.getView().addDependent(this._oPopover);
+			}
+			return this._oPopover;
+		},
+
+		handleTitlePress : function (oEvent) {
+			var domRef = oEvent.getParameter("domRef");
+			this._getPopover().openBy(domRef);
 		}
 	});
 });
